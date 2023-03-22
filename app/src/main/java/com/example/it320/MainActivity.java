0c52c3ddc42c522c9d96c0e9ac0f3ace89400f9d
Ghadeerabdu <ghadeeralmuhaideb@gmail.com>
2023-03-22 10:33:25 +0300

    hello

---
+++ app/src/main/java/com/example/it320/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.it320;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+heloo
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
